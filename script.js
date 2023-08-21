a40ab1879fe6a31c05f0cc2c25517b98c9a5b4fd
Rossamylne <l.bader26@yahoo.com>
2023-08-22 00:13:06 +0200

    Finish the form validation and start to create a delete button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const mainContainer = document.querySelector(".main-container");
 
 
 function Book(name, author, page, read) {
-    this.name = name;
+    this.title = name;
     this.author = author;
     this.page = page;
     this.read = read;
@@ -15,25 +15,22 @@ function addBookToLibrary(book, library) {
     return library;
 }
 
-
-var myBook = new Book("caca", "pipi", 12, false);
-myLibrary = addBookToLibrary(myBook, myLibrary);
-
-var secondBook = new Book("aezer", "vbvbvcb", 19, true);
-myLibrary = addBookToLibrary(secondBook, myLibrary);
-
-
-if (myLibrary) {
-    myLibrary.forEach(inBook => {
+function addToDOM(library) {
+    if (mainContainer.lastElementChild) {
+        while (mainContainer.lastElementChild) {
+            mainContainer.removeChild(mainContainer.lastElementChild);
+        }
+    }
+    library.forEach(inBook => {
         const div = document.createElement('div');
         div.classList.add('bookcard');
-        console.log(inBook);
+        div.setAttribute('id', inBook["title"]);
         for (const key in inBook) {
+            console.log(div.getAttribute('id'));
             var content = ""; 
             if (key !== "read") {
                 content = document.createElement('p');
                 content.textContent = `${key} : ${inBook[key]}`;
-                console.log(`${key} : ${inBook[key]}`);
             } else if(key === "read"){
                 content = document.createElement('button');
                 content.classList.add('read-button');
@@ -53,6 +50,58 @@ if (myLibrary) {
             }
             div.appendChild(content);
         }
+        const deleteButton = document.createElement('button');
+            deleteButton.classList.add('remove-button');
+            deleteButton.textContent = "Remove";
+            deleteButton.addEventListener('click', () => {
+                mainContainer.removeChild(mainContainer.child);
+            })
+        div.appendChild(deleteButton);
         mainContainer.appendChild(div);
     });
 }
+
+
+const addBookFormContainer = document.querySelector(".add-book-form-container");
+const addBookButton = document.querySelector("#addBook");
+const cancelButton = document.querySelector("#cancel-button");
+const submitButton = document.querySelector("#submit-button");
+
+
+
+
+
+addBookButton.addEventListener('click', () => {
+    addBookFormContainer.style.cssText = "visibility: visible";
+});
+
+cancelButton.addEventListener('click', () => {
+    addBookFormContainer.style.cssText = "visibility: hidden";
+});
+
+
+submitButton.addEventListener('click', () => {
+    const bookName = document.querySelector("#book_name").value;
+    const bookAuthor = document.querySelector("#book_author").value;
+    const bookPage = document.querySelector("#book_page").value;
+    var bookRead = document.querySelector("#already_read").checked;
+    console.log(bookRead);
+    switch(bookRead) {
+        case "on":
+            bookRead = true;
+            break;
+        case "off":
+            bookRead = false;
+    }
+    if (bookName && bookAuthor && bookPage) {
+        var newBook = new Book(bookName, bookAuthor, bookPage, bookRead);
+        myLibrary = addBookToLibrary(newBook, myLibrary);
+        addBookFormContainer.style.cssText = "visibility: hidden";
+        addToDOM(myLibrary);
+    }
+})
+
+
+
+
+
