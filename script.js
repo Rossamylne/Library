c0ce278bace5fd7bfcf4c23664d93cb311525bde
Rossamylne <l.bader26@yahoo.com>
2023-08-22 15:51:08 +0200

    Finish the project, finish remove button and add the change reading status feature

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function Book(name, author, page, read) {
     return this;
 }
 
+
 function addBookToLibrary(book, library) {
     library.push(book);
     return library;
@@ -24,14 +25,15 @@ function addToDOM(library) {
     library.forEach(inBook => {
         const div = document.createElement('div');
         div.classList.add('bookcard');
-        div.setAttribute('id', inBook["title"]);
         for (const key in inBook) {
-            console.log(div.getAttribute('id'));
             var content = ""; 
-            if (key !== "read") {
+            if (key == "title" || key == "author" || key == "page") {
                 content = document.createElement('p');
                 content.textContent = `${key} : ${inBook[key]}`;
-            } else if(key === "read"){
+            } else if (key.includes("change")) {
+                break;
+            } 
+            else if(key === "read"){
                 content = document.createElement('button');
                 content.classList.add('read-button');
                 switch(inBook[key]) {
@@ -47,6 +49,19 @@ function addToDOM(library) {
                         div.style.cssText = "border: 1px solid #EA526F";
                         break;
                 }
+                content.addEventListener('click', function() {
+                    if (inBook[key] ===true) {
+                        content.style.cssText = "background-color: #EA526F;"
+                        content.textContent = "Not read";
+                        div.style.cssText = "border: 1px solid #EA526F";
+ 
+                    } else if (inBook[key] === false) {
+                        content.style.cssText = "background-color: #80ed99;";
+                        content.textContent = "Read";
+                        div.style.cssText = "border: 1px solid #80ed99";
+                    }
+                    inBook[key] = !inBook[key];
+                })
             }
             div.appendChild(content);
         }
@@ -54,7 +69,11 @@ function addToDOM(library) {
             deleteButton.classList.add('remove-button');
             deleteButton.textContent = "Remove";
             deleteButton.addEventListener('click', () => {
-                mainContainer.removeChild(mainContainer.child);
+                div.style.cssText = "display: none;";
+                const index = library.indexOf(inBook);
+                if (index > -1) {
+                    library.splice(index, 1);
+                }
             })
         div.appendChild(deleteButton);
         mainContainer.appendChild(div);
@@ -85,7 +104,6 @@ submitButton.addEventListener('click', () => {
     const bookAuthor = document.querySelector("#book_author").value;
     const bookPage = document.querySelector("#book_page").value;
     var bookRead = document.querySelector("#already_read").checked;
-    console.log(bookRead);
     switch(bookRead) {
         case "on":
             bookRead = true;
